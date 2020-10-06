136c5399509f1c043b29e1a048c7876790dc9c56
Andrea Romeo <andrea.romeo@thalesgroup.com>
2020-10-06 16:16:10 +0200

    tmp committ to delete

---
+++ Ordering.API/Application/IntegrationEvents/Events/OrderPaymentFailedIntegrationEvent .cs
@@ -0,0 +1,11 @@
+﻿namespace Ordering.API.Application.IntegrationEvents.Events
+{
+    using Thales.BuildingBlocks.EventBus.Events;
+
+    public class OrderPaymentFailedIntegrationEvent : IntegrationEvent
+    {
+        public int OrderId { get; }
+
+        public OrderPaymentFailedIntegrationEvent(int orderId) => OrderId = orderId;
+    }
+}
---
+++ Ordering.API/Infrastructure/AutofacModules/ApplicationModule.cs
@@ -0,0 +1,50 @@
+﻿using Autofac;
+using Thales.BuildingBlocks.EventBus.Abstractions;
+using Thales.Services.Ordering.API.Application.Commands;
+using Thales.Services.Ordering.API.Application.Queries;
+using Thales.Services.Ordering.Domain.AggregatesModel.BuyerAggregate;
+using Thales.Services.Ordering.Domain.AggregatesModel.OrderAggregate;
+using Thales.Services.Ordering.Infrastructure.Idempotency;
+using Thales.Services.Ordering.Infrastructure.Repositories;
+using System.Reflection;
+
+namespace Thales.Services.Ordering.API.Infrastructure.AutofacModules
+{
+
+    public class ApplicationModule
+        :Autofac.Module
+    {
+
+        public string QueriesConnectionString { get; }
+
+        public ApplicationModule(string qconstr)
+        {
+            QueriesConnectionString = qconstr;
+
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+
+            builder.Register(c => new OrderQueries(QueriesConnectionString))
+                .As<IOrderQueries>()
+                .InstancePerLifetimeScope();
+
+            builder.RegisterType<BuyerRepository>()
+                .As<IBuyerRepository>()
+                .InstancePerLifetimeScope();
+
+            builder.RegisterType<OrderRepository>()
+                .As<IOrderRepository>()
+                .InstancePerLifetimeScope();
+
+            builder.RegisterType<RequestManager>()
+               .As<IRequestManager>()
+               .InstancePerLifetimeScope();
+
+            builder.RegisterAssemblyTypes(typeof(CreateOrderCommandHandler).GetTypeInfo().Assembly)
+                .AsClosedTypesOf(typeof(IIntegrationEventHandler<>));
+
+        }
+    }
+}
